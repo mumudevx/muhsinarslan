c8215413e23baa5d3f56d74d4c28625ac3136c74
Muhsin <mumudevx@gmail.com>
2025-04-29 12:39:02 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,25 @@ export default defineNuxtConfig({
         lang: "en",
       },
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+      script: [
+        // Google tag (gtag.js) - Async Script
+        {
+          src: "https://www.googletagmanager.com/gtag/js?id=G-WZGLHRF43F",
+          async: true,
+          key: 'gtag-js-async' // Use key for uniqueness
+        },
+        // Google tag (gtag.js) - Inline Script
+        {
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WZGLHRF43F');
+          `,
+          type: 'text/javascript',
+          key: 'gtag-js-inline' // Use key for uniqueness
+        }
+      ]
     },
   },
   ssr: false,
